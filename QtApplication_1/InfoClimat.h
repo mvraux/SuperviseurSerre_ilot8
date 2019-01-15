41708897022245208ff652b0881bd4f79fee22fd
yweyer <yweyer@btslivh.eu>
2019-01-15 15:04:23 +0100

    commit vueSerre

---
+++ QtApplication_1/InfoClimat.h
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   InfoClimat.h
+ * Author: snir2g2
+ *
+ * Created on 15 janvier 2019, 14:49
+ */
+
+#ifndef INFOCLIMAT_H
+#define INFOCLIMAT_H
+
+class InfoClimat {
+public:
+    InfoClimat();
+    InfoClimat(const InfoClimat& orig);
+    virtual ~InfoClimat();
+private:
+    int hygrometrie;
+    int temperature;
+    int positionOuvrants;
+    int luminosite;
+    int date;
+};
+
+#endif /* INFOCLIMAT_H */
+
